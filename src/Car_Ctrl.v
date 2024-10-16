ecd062d86d7fa445c01a72db5f5afd9a793f4a3a
Lucas <lucas.megnan@algosup.com>
2024-10-16 17:00:06 +0200

    Change comments in english

--- src/Car_Ctrl.v
+++ src/Car_Ctrl.v
@@ -1,28 +1,28 @@
 module car_Ctrl 
-  #(parameter c_GAME_WIDTH=640, // Résolution horizontale du jeu
-    parameter c_GAME_HEIGHT=480, // Résolution verticale du jeu
-    parameter c_initial_position = 0, // Position initiale de la voiture (0 pour la gauche, c_GAME_WIDTH - 1 pour la droite)
-    parameter c_direction = 0,   // Direction de déplacement (0 = droite, 1 = gauche)
-    parameter c_car_SPEED = 1650000,  // Vitesse de la voiture
-    parameter c_CAR_WIDTH = 32,  // Largeur de la voiture paramétrée
-    parameter c_CAR_HEIGHT = 32) // Hauteur de la voiture paramétrée
+  #(parameter c_GAME_WIDTH=640, // Resolution of the game (Horizontal)
+    parameter c_GAME_HEIGHT=480, // Résolution of the game (Vertical)
+    parameter c_initial_position = 0, // initial position of the car (0 for the left, c_GAME_WIDTH - 1 for the right)
+    parameter c_direction = 0,   // deplacement direction (0 = right, 1 = left)
+    parameter c_car_SPEED = 1650000,  // speed of the car
+    parameter c_CAR_WIDTH = 32,  // Width of the car setting up
+    parameter c_CAR_HEIGHT = 32) // High of the car setting up
   (
     input            i_Clk,
     input            i_Game_Active,
-    input [9:0]      i_Col_Count_Div, // Compteur de colonnes (10 bits)
-    input [9:0]      i_Row_Count_Div, // Compteur de lignes (10 bits)
-    input [9:0]      i_car_Y,         // Position verticale (Y) de la voiture
-    output reg       o_Draw_car,      // Indicateur pour dessiner la voiture
-    output reg [9:0] o_car_X = 0,     // Position horizontale (X) de la voiture
-    output reg [9:0] o_car_Y = 0      // Position verticale (Y) de la voiture
+    input [9:0]      i_Col_Count_Div, // Columns counter (10 bits)
+    input [9:0]      i_Row_Count_Div, // Lines counter(10 bits)
+    input [9:0]      i_car_Y,         // Vertical position (Y) of the car
+    output reg       o_Draw_car,      // Indicator to draw the car
+    output reg [9:0] o_car_X = 0,     // Horizontal position (X) of the car
+    output reg [9:0] o_car_Y = 0      // Vertical position (Y) of the car
   );
 
-  reg [31:0] r_car_Count = 0; // Compteur pour contrôler la vitesse du mouvement
-  reg [9:0] r_car_X_Prev = 0; // Position précédente en X de la voiture
+  reg [31:0] r_car_Count = 0; // Counter to manage the car speed
+  reg [9:0] r_car_X_Prev = 0; // Previous position of the car
 
   always @(posedge i_Clk)
   begin
-    // Si le jeu n'est pas actif, la voiture reste à la position initiale
+    // If the game is not ON, the car is set to the initial position
     if (i_Game_Active == 1'b0)
     begin
       o_car_X      <= c_initial_position; 
@@ -31,7 +31,7 @@ module car_Ctrl
     end
     else
     begin
-      // Gestion du compteur pour la vitesse de la voiture
+      // Manage the counter to control the car speed
       if (r_car_Count < c_car_SPEED)
         r_car_Count <= r_car_Count + 1;
       else
@@ -40,15 +40,15 @@ module car_Ctrl
 
         r_car_X_Prev <= o_car_X;
         
-        // Déplacement de la voiture
-        if (c_direction == 0)  // Si la direction est 0, la voiture se déplace vers la droite
+        // Car deplacement
+        if (c_direction == 0)  // If the direction is 0, the car moves to the right
         begin
           if (o_car_X == c_GAME_WIDTH - 1)
             o_car_X <= 0; 
           else
             o_car_X <= o_car_X + 1; 
         end
-        else  // Si la direction est 1, la voiture se déplace vers la gauche
+        else  // If the direction is 1, the car moves to the left
         begin
           if (o_car_X == 0)
             o_car_X <= c_GAME_WIDTH - 1; 
@@ -59,15 +59,15 @@ module car_Ctrl
     end
   end
 
-  // Dessine la voiture en fonction des coordonnées X et Y, avec une largeur et hauteur paramétrables
+  // Draw the car in the function of the coordinates X and Y, with the width and height 
   always @(posedge i_Clk)
   begin
-    // Vérifie si la voiture doit être dessinée en fonction de sa position X et Y
+    // If the car is in the range of the coordinates X and Y, the car is drawn
     if (i_Col_Count_Div >= o_car_X && i_Col_Count_Div < o_car_X + c_CAR_WIDTH && 
         i_Row_Count_Div >= o_car_Y && i_Row_Count_Div < o_car_Y + c_CAR_HEIGHT) 
-      o_Draw_car <= 1'b1; // La voiture est dessinée
+      o_Draw_car <= 1'b1; // The car is drawn
     else
-      o_Draw_car <= 1'b0; // La voiture n'est pas dessinée
+      o_Draw_car <= 1'b0; // The car is not drawn
   end
 
 endmodule // car_Ctrl
--- src/Sync_To_Count.v
+++ src/Sync_To_Count.v
@@ -1,53 +1,53 @@
 module Sync_To_Count 
- #(parameter TOTAL_COLS = 800,   // Total de colonnes (incluant les porches)
-   parameter TOTAL_ROWS = 525)   // Total de lignes (incluant les porches)
-  (input            i_Clk,       // Horloge principale
-   input            i_HSync,     // Signal de synchronisation horizontale
-   input            i_VSync,     // Signal de synchronisation verticale
-   output reg       o_HSync = 0, // Sortie pour synchronisation horizontale
-   output reg       o_VSync = 0, // Sortie pour synchronisation verticale
-   output reg [9:0] o_Col_Count = 0,  // Compteur de colonnes
-   output reg [9:0] o_Row_Count = 0); // Compteur de lignes
+ #(parameter TOTAL_COLS = 800,   // Total of columns (inclued porches)
+   parameter TOTAL_ROWS = 525)   // Total of lines (inclued porches)
+  (input            i_Clk,       // Principal Clock
+   input            i_HSync,     // Signal synchronisation horizontal
+   input            i_VSync,     // Signal synchronisation vertical
+   output reg       o_HSync = 0, // Exit for synchronisation horizontal
+   output reg       o_VSync = 0, // Exit for synchronisation vertical
+   output reg [9:0] o_Col_Count = 0,  // Counter of columns
+   output reg [9:0] o_Row_Count = 0); // Counter of lines
    
-   wire w_Frame_Start;  // Signal pour détecter le début d'une nouvelle frame
+   wire w_Frame_Start;  // Signal to detect the start of the frame
    
-   // Registre de synchronisation pour aligner avec les données en sortie
+   // Synchronisation register to align the exit signals
    always @(posedge i_Clk)
    begin
      o_VSync <= i_VSync;
      o_HSync <= i_HSync;
    end
 
-   // Suivre les compteurs de lignes et de colonnes
+   // Follow the counters of columns and lines
    always @(posedge i_Clk)
    begin
      if (w_Frame_Start == 1'b1)
      begin
-       o_Col_Count <= 0;  // Réinitialisation du compteur de colonnes
-       o_Row_Count <= 0;  // Réinitialisation du compteur de lignes
+       o_Col_Count <= 0;  // Reset the counter of columns
+       o_Row_Count <= 0;  // Reset the counter of lines
      end
      else
      begin
-       if (o_Col_Count == TOTAL_COLS-1)  // Si on atteint la fin de la ligne
+       if (o_Col_Count == TOTAL_COLS-1)  // If we reach the end of the frame
        begin
-         if (o_Row_Count == TOTAL_ROWS-1)  // Si on atteint la fin de la frame
+         if (o_Row_Count == TOTAL_ROWS-1)  // If we reach the end of the frame
          begin
-           o_Row_Count <= 0;  // Réinitialisation du compteur de lignes
+           o_Row_Count <= 0;  // Reset the counter of lines
          end
          else
          begin
-           o_Row_Count <= o_Row_Count + 1;  // Incrémentation des lignes
+           o_Row_Count <= o_Row_Count + 1;  // Increment the lines
          end
-         o_Col_Count <= 0;  // Réinitialisation du compteur de colonnes
+         o_Col_Count <= 0;  // Reset the counter of columns
        end
        else
        begin
-         o_Col_Count <= o_Col_Count + 1;  // Incrémentation des colonnes
+         o_Col_Count <= o_Col_Count + 1;  // Increment the columns
        end
      end
    end
 
-   // Détection de la montée du signal de synchronisation verticale pour réinitialiser les compteurs
+   // Detection of the rise of the vertical sync signal to reset the counters
    assign w_Frame_Start = (~o_VSync & i_VSync);
 
 endmodule
